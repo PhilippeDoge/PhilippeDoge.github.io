7275205b7687bee4c312a62941baadbc8a621996
PhilippeDoge <Philippejacobo91@gmail.com>
2023-05-02 23:48:28 +0200

    initial commit

---
+++ créer compte.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Créer votre compte</title>
+</head>
+<body>
+	<a href="accueil.html">Retour à la page d'accueil</a>
+	<h1> Créer votre compte</h1>
+	<p>Veuillez saisir votre pseudo:</p>
+	<input type="text" size="30" name="pseudo" placeholder="Votre pseudo">
+	<p>Veuillez saisir votre adresse email:</p>
+	<input type="text" size="30" name="email" placeholder="Votre email">
+	<p>Veuillez saisir votre mot de passe:</p>
+	<input type="password" size="30" name="mdp" placeholder="Votre mot de passe">
+	<p>Confirmez votre mot de passe:</p>
+	<input type="password" size="30" name="mdp2" placeholder="Votre mot de passe">
+	<p>Vérifiez que vous êtes un humain. Saisissez "Je ne suis pas un bot":</p>
+	<input type="text" size="30" name="bot" placeholder="Votre réponse">
+	<p>Envoyer le formulaire:</p>
+	<input type="submit" name="send" value="Envoyer">
+</body>
+</html>
