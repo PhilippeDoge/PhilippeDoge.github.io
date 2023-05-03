0a2d86d5d0f18f71ee1d8536a6dd177a34e69542
Suisimp1101 <132386077+Suisimp1101@users.noreply.github.com>
2023-05-03 16:35:44 +0200

    Update mdp oublie.php

--- mdp oublie.php
+++ mdp oublie.php
@@ -6,6 +6,29 @@
 </head>
 <body>
 	<a href="index.php">Retour à la page de connexion</a>
-	<h1>Future page de mdp oublié</h1>
+	<h1>Mot de passe oublié</h1>
+	<form method="POST" action="">
+		<label for="mail">Veuillez saisir votre adresse mail:</label>
+		<input type="email" id="mail" name="mail" required><br>
+
+		<input type="submit" name="envoyer" value="Envoyer">
+	</form>
+	<?php
+		if(isset($_POST['envoyer'])){
+
+			//Récupère les données
+			$mail=$_POST['mail'];
+
+			//Vérifie si le mail est présent dans la base de données
+			if($mail=='jmlesenfants@pdo.com'){
+				//Renvoie vers la page de réinitialisation
+				header("Location: reset mdp.php");
+				exit();
+			}else{
+				//Renvoie à l'utilisateur que le mail renseigné n'existe pas
+				echo "L'adresse mail n'existe pas. Veuillez réessayer.";
+			}
+		}
+	?>
 </body>
 </html>
