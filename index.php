d01aa6c90e30f203fd5513f12472e564cf5f9af5
Suisimp1101 <132386077+Suisimp1101@users.noreply.github.com>
2023-05-04 05:34:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,33 +9,50 @@
 	<h1> Bienvenue sur la page de connexion</h1>
 	<form method="POST" action="">
 		<label for="pseudo">Saisissez votre pseudo:</label>
-		<input type="text" id="pseudo" name="pseudo" required><br>
+        <input type="text" id="pseudo" name="pseudo" required><br>
 
-            	<label for="mdp">Saisissez votre mot de passe:</label>
-            	<input type="password" id="mdp" name="mdp" required><br>
+        <label for="mdp">Saisissez votre mot de passe:</label>
+        <input type="password" id="mdp" name="mdp" required><br>
 
-            	<input type="submit" name="envoyer" value="Envoyer">
-	    
-	        <a href="creer compte.php">Créer votre compte</a><br>
-   		<a href="mdp oublie.php">Mot de passe oublié?</a>
-   	 </form>
-   	 <?php
+        <input type="submit" name="envoyer" value="Envoyer"> 
+
+        <a href="creer compte.php">Créer votre compte</a><br>
+
+    	<a href="mdp oublie.php">Mot de passe oublié?</a>
+
+    </form>
+
+    <?php
+   		require_once "connect.php";
+
 		// Vérifie si le formulaire a été soumis
 		if (isset($_POST['envoyer'])) {
 
-    		// Récupération des données du formulaire
-    		$pseudo = $_POST['pseudo'];
-    		$mdp = $_POST['mdp'];
+    		// Récupère les données du formulaire
+    		$pseudo=$_POST['pseudo'];
+    		$mdp=$_POST['mdp'];
 
-   			// Traitement des données
-    		if ($pseudo == 'admin' && $mdp == '1234') {
-        		// Si l'identifiant et mot de passe sont corrects, redirige vers la page d'accueil
+    		// Échappe les caractères spéciaux pour éviter les injections SQL
+    		$pseudo=mysqli_real_escape_string($conn,$pseudo);
+    		$mdp=mysqli_real_escape_string($conn,$mdp);
+
+    		//Traite les données
+    		$select="SELECT pseudo,mdp FROM user WHERE pseudo='$pseudo' AND mdp='$mdp';";
+
+    		$result=mysqli_query($conn,$select);
+
+    		//Si on trouve un résultat, renvoie vers la page d'accueil
+    		if(mysqli_num_rows($result)>0){
     			header("Location: accueil.php");
     			exit();
     		}else{
-        		// Si les identifiants sont incorrects, afficher un message d'erreur
-        		echo "<div class='erreur'> Identifiant ou mot de passe incorrect. </div>";
+    			//Sinon renvoie un message d' erreur
+    			echo "<div class='erreur'> Identifiant ou mot de passe incorrect. </div>";
     		}
+
+    		//Ferme la connexion à la base de données
+            mysqli_close($conn);
+
 		}
 	?>
 </body>
