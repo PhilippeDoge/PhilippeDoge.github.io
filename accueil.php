d90f3511481e02afff33d5799e3b8ec0f3c157ef
Suisimp1101 <132386077+Suisimp1101@users.noreply.github.com>
2023-05-05 19:01:46 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -7,17 +7,43 @@
 </head>
 <body>
 	<form method="POST" action="">
+
+
 		<input type="submit" name="deco" value="Déconnexion">
+
+		<input type="submit" name="recherche" value="Rechercher des utilisateurs";>
+
+		<input type="submit" name="profil" value="Votre profil">
 
 	</form>
 	<?php
+		require_once "connect.php";
+		
+		//Récupère le pseudo
+		$pseudo=$_GET['pseudo'];
+
+		//Renvoie vers la page de connexion
 		if(isset($_POST['deco'])){
+			session_destroy();
 			header("Location: index.php");
 			exit();
 		}
 
+		if(isset($_POST['recherche'])){
+			header("Location: recherche.php?pseudo=$pseudo");
+			exit();
+		}
+
+		//Renvoie vers le profil
+		if(isset($_POST['profil'])){
+			header("Location: profil perso.php?pseudo=$pseudo");
+			exit();
+		}
+
+
 	?>
-	<h1>Ceci est une page d'accueil. Maintenance en cours...</h1>
+	<h1>Accueil</h1>
+	<h2>Bienvenue <?php echo "$pseudo";?> !</h2>
 
 </body>
 </html>
