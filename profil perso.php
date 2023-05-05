c1fcc63f6a8a0e37f03d5eda0b2508efb4e2d71f
Suisimp1101 <132386077+Suisimp1101@users.noreply.github.com>
2023-05-05 14:49:00 +0200

    Add files via upload

---
+++ profil perso.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Profil personnel</title>
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+	<h1>Votre profil</h1>
+	<?php
+		require_once "connect.php";
+		$pseudo=$_GET['pseudo'];
+		echo "<a href=\"accueil.php?pseudo=".$pseudo."\">Retour en arrière</a>";
+
+		/*Récupère les données
+		$getinfo="SELECT * FROM profil WHERE pseudo='$pseudo';";
+		$data=mysqli_query($conn,$getinfo);
+		$row=mysqli_fetch_assoc($data);
+		$followers=$row['followers'];
+		$publications=$row['publications'];
+		$admin=$row['admin'];
+		*/
+	?>
+
+	<form method="POST" action="">
+
+		<input type="submit" name="mdp" value="Changer le mot de passe"> 
+	</form>
+	<?php
+
+		if(isset($_POST['mdp'])){
+			header("Location:modif mdp.php?pseudo=$pseudo");
+			exit;
+		}
+
+		?>
+
+
+
+
+</body>
+</html>
