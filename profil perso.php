fba44441eb9c91f9159bd4a3dd111d6273aa6546
Suisimp1101 <132386077+Suisimp1101@users.noreply.github.com>
2023-05-06 15:48:30 +0200

    Update profil perso.php

--- profil perso.php
+++ profil perso.php
@@ -2,38 +2,40 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Profil personnel</title>
+	<title>Votre profil</title>
 	<link href="style.css" rel="stylesheet">
 </head>
 <body>
 	<h1>Votre profil</h1>
 	<?php
+		session_start();
 		require_once "connect.php";
-		$pseudo=$_GET['pseudo'];
-		echo "<a href=\"accueil.php?pseudo=".$pseudo."\">Retour en arrière</a>";
+
+		//Renvoie vers la page d'accueil
+		echo "<a href=\"accueil.php\">Retour en arrière</a>";
 
 		//Récupère les données
-		$getinfo="SELECT * FROM profil WHERE pseudo='$pseudo';";
-		$data=mysqli_query($conn,$getinfo);
+		$select="SELECT * FROM profil WHERE pseudo='{$_SESSION['pseudo']}';";
+		$data=mysqli_query($conn,$select);
 		$row=mysqli_fetch_assoc($data);
 		$followers=$row['followers'];
 		$publications=$row['publications'];
 		$admin=$row['admin'];
-
 	?>
 
 	<form method="POST" action="">
 
-		<p>Pseudo:<?php echo "$pseudo";?></p><br>
-		<p>Followers:<?php echo "$followers";?></p><br>
+		<p>Pseudo:<?php echo $_SESSION['pseudo'];?></p>
+		<p>Followers:<?php echo "$followers";?></p>
 		<p>Publications:<?php echo "$publications";?></p>
+		<p>Admin:<?php if($admin==0){echo "Non";}else{echo "Oui";};?></p>
 
 		<input type="submit" name="mdp" value="Changer le mot de passe"> 
 	</form>
 	<?php
 
 		if(isset($_POST['mdp'])){
-			header("Location:modif mdp.php?pseudo=$pseudo");
+			header("Location:modif mdp.php");
 			exit;
 		}
 
