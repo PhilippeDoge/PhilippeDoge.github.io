a0affee14ae672c9820662f229bc11bda1f3569b
Suisimp1101 <132386077+Suisimp1101@users.noreply.github.com>
2023-05-06 17:29:44 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -2,48 +2,72 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Page d'accueil</title>
+	<title>My Page</title>
 	<link href="style.css" rel="stylesheet">
 </head>
 <body>
-	<form method="POST" action="">
 
-
-		<input type="submit" name="deco" value="Déconnexion">
-
-		<input type="submit" name="recherche" value="Rechercher des utilisateurs";>
-
-		<input type="submit" name="profil" value="Votre profil">
-
+    <div class="container">
+	<form method="POST" class='deco' action="">
+			<input type="submit" name="deco" value="Déconnexion"><br>
+			<input type="submit" name="profil" value="Votre profil">
 	</form>
-	<?php
-		require_once "connect.php";
-		
-		//Récupère le pseudo
-		$pseudo=$_GET['pseudo'];
+        <div>
 
-		//Renvoie vers la page de connexion
-		if(isset($_POST['deco'])){
-			session_destroy();
-			header("Location: index.php");
-			exit();
-		}
+            <!-- Barre de recherche -->
+            <form class ="recherche" method="POST" action="">
 
-		if(isset($_POST['recherche'])){
-			header("Location: recherche.php?pseudo=$pseudo");
-			exit();
-		}
+                <input type="text" id="pseudo" name="pseudo">
 
-		//Renvoie vers le profil
-		if(isset($_POST['profil'])){
-			header("Location: profil perso.php?pseudo=$pseudo");
-			exit();
-		}
+                <input type="submit" name="rechercher" value="Rechercher">
+            </form>
+
+            <!-- Création de post -->
+            <h1>Racontes nous ta vie</h1>
+            <form class='creation' method="POST" action="">
+                <label for="content">Contenu</label><br>
+                <textarea type="text" id="content" name="content"></textarea><br>
+
+                <input type="submit" name="post" value="Publier">
+
+            </form>
 
 
-	?>
-	<h1>Accueil</h1>
-	<h2>Bienvenue <?php echo "$pseudo";?> !</h2>
+            <!-- Retour page profil -->
+            
+        <?php
+        	session_start();
+        	if(isset($_POST['post'])){
+        		require_once "creer post.php";
 
+        	}
+
+        	if(isset($_POST['rechercher'])){
+        		require_once "recherche.php";
+        	}
+
+        	if(isset($_POST['deco'])){
+        		header("Location: index.php");
+       			exit();
+       		}
+
+        	if(isset($_POST['profil'])){
+        		header("Location: profil perso.php");
+        		exit();
+        		}
+        	?>
+        </div>
+
+        
+        <div class="middle">
+            <?php
+            require_once "voir poste.php";
+            ?>
+        </div>
+
+        <div>
+            Pub de merde
+        </div>
+    </div>
 </body>
 </html>
