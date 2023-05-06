61b5c8ceadc37414ac7b79c63023e4227ae77aa8
Suisimp1101 <132386077+Suisimp1101@users.noreply.github.com>
2023-05-06 22:42:55 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 	<?php
-	session_start();
+		session_start();
 		require_once "connect.php";
 		echo "<h1>Profil de ".$_SESSION['pseudo_rec']."</h1>";
 		//Récupère les données
@@ -17,16 +17,37 @@
 		$followers=$row['followers'];
 		$publications=$row['publications'];
 		$admin=$row['admin'];
-	?>
+		$prive=$row['prive'];
 
-	<a href="accueil.php">Retour à la page d'accueil</a>
-	<form method="POST" action="">
-		<p>Pseudo:<?php echo $_SESSION['pseudo_rec'];?></p>
-		<p>Followers:<?php echo $followers;?></p>
-		<p>Publications:<?php echo $publications;?></p>
-		<p>Admin:<?php if($admin==0){echo "Non";}else{echo "Oui";};?></p>
+		//Récupère l'id de l'utilisateur recherché
+		$sel_id="SELECT id FROM user WHERE pseudo='{$_SESSION['pseudo_rec']}';";
+		$getid=mysqli_query($conn,$sel_id);
+		$result = mysqli_fetch_assoc($getid);
+		$_SESSION['idp'] = $result['id'];
+
+		//Vérifie si l'utilisateur est déjà suivi ou non
+		$select="SELECT * FROM follower WHERE follower_id={$_SESSION['id']} AND followed_id={$_SESSION['idp']}";
+		$result2=mysqli_query($conn,$select);
+
+
+		echo "<a href=\"accueil.php\">Retour à la page d'accueil</a>";
+		echo "<form method=\"POST\" action=\"\">";
+		if($prive==true && mysqli_num_rows($result2)==0){
+			echo "Le profil de l'utilisateur est privé. Veuillez le suivre afin de voir ses publications";
+		}else{
+			echo "<p>Pseudo:".$_SESSION['pseudo_rec']."</p>";
+			echo "<p>Followers:".$followers."</p>";
+			echo "<p>Publications:".$publications."</p>";
+			if($prive==false){
+				echo "Privé: Non";
+			}else{
+				echo "Privé: Oui";
+			}
+		}
+	?>
 		<input type="submit" name="suivre" value="Suivre"> 
-	</form>
+		</form>
+		
 
 	<?php
 		require_once "connect.php";
@@ -34,27 +55,38 @@
 		if(isset($_POST['suivre'])){
 
 
-			//Récupère l'id de l'utilisateur recherché
-			$sel_id="SELECT id FROM user WHERE pseudo='{$_SESSION['pseudo_rec']}';";
-			$getid=mysqli_query($conn,$sel_id);
-			$result = mysqli_fetch_assoc($getid);
-			$_SESSION['idp'] = $result['id'];
 
-			//Vérifie si l'utilisateur est déjà suivi ou non
-			$select="SELECT * FROM follower WHERE follower_id={$_SESSION['id']} AND followed_id={$_SESSION['idp']}";
-			$result2=mysqli_query($conn,$select);
 
 			if(mysqli_num_rows($result2)>0){
     			echo "<div class='erreur'>Utilisateur déjà suivi.</div>";
 			}else{
-    			$update_p="UPDATE profil SET followers='$followers'+1 WHERE pseudo='{$_SESSION['pseudo_rec']}';";
+    			$update_p="UPDATE profil SET followers=followers WHERE pseudo='{$_SESSION['pseudo_rec']}';";
     			$insert="INSERT INTO follower(follower_id,followed_id) VALUES ('{$_SESSION['id']}','{$_SESSION['idp']}');";
     			mysqli_query($conn,$update_p);
     			mysqli_query($conn,$insert);
     			echo "<div class='succes'>Utilisateur suivi</div>";
 				}
 			}
+
 		?>
+		
+	<?php
+        require_once "connect.php";
+
+        $getpost = "SELECT posts.content, user.pseudo 
+        FROM posts 
+        INNER JOIN user 
+        ON posts.user_id = user.id 
+        WHERE user.pseudo='{$_SESSION['pseudo_rec']}' 
+        ORDER BY posts.id DESC";
+        $result = mysqli_query($conn, $getpost);
+
+        while ($row = mysqli_fetch_assoc($result)) { ?>
+            <div class='post'>
+                <p>Posted by <?php echo $row['pseudo']; ?> : <?php echo $row['content']; ?></p>
+            </div>
+        <?php }
+    ?>
 
 </body>
 </html>
