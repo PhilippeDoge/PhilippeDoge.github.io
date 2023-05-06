097b4447c0114311f8f0651e785c5410622ca230
PhilippeDoge <132386082+PhilippeDoge@users.noreply.github.com>
2023-05-06 15:28:57 +0200

    Update voir poste.php

--- voir poste.php
+++ voir poste.php
@@ -6,47 +6,40 @@
 	<link href="style.css" rel="stylesheet">
 </head>
 <body>
-<?php
-    session_start();
-    require_once "connect.php";
+    <?php
+        require_once "connect.php";
 
-    // Retrieve the list of users the current user follows
-    $user_id = $_SESSION['id'];
-    $query = "SELECT followed_id FROM follower WHERE follower_id = ?";
-    $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, "i", $user_id);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
+        
+        // Retrieve the list of users the current user follows
+        $user_id = $_SESSION['id'];
+        $query = "SELECT followed_id FROM follower WHERE follower_id = ?";
+        $stmt = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($stmt, "i", $user_id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
 
-    // Extract the followed users' IDs into an array
-    $followed_users = array();
-    while ($row = mysqli_fetch_assoc($result)) {
-        $followed_users[] = $row['followed_id'];
-    }
+        // Extract the followed users' IDs into an array
+        $followed_users = array();
+        while ($row = mysqli_fetch_assoc($result)) {
+            $followed_users[] = $row['followed_id'];
+        }
 
-    // Retrieve the posts from the followed users
-    if (count($followed_users) > 0) {
-        $followed_users_str = implode(',', $followed_users);
-        $query = "SELECT posts.content, user.pseudo FROM posts 
-                  INNER JOIN user ON posts.user_id = user.id 
-                  WHERE user_id IN ($followed_users_str)";
-        $result = mysqli_query($conn, $query);
-    }
-?>
+        // Retrieve the posts from the followed users, including the current user
+        if (count($followed_users) > 0) {
+            $followed_users_str = implode(',', $followed_users);
+            $query = "SELECT posts.content, user.pseudo FROM posts 
+                    INNER JOIN user ON posts.user_id = user.id 
+                    WHERE user_id IN ($followed_users_str)
+                    ORDER BY posts.id DESC"; // Order posts by ID in descending order
+            $result = mysqli_query($conn, $query);
+        }
+    ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Posts from followed users</title>
-</head>
-<body>
     <h1>Posts from followed users</h1>
     <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-        <div>
-            <p>Posted by <?php echo $row['pseudo']; ?><?php echo $row['content']; ?></p>
+        <div class='post'>
+            <p>Posted by <?php echo $row['pseudo']; ?> : <?php echo $row['content']; ?></p>
         </div>
     <?php } ?>
 </body>
-</html>
-
 </html>
