b69cf45e118677faa40125e03f56b8423c81bde6
Suisimp1101 <132386077+Suisimp1101@users.noreply.github.com>
2023-05-06 17:29:08 +0200

    Update profil perso.php

--- profil perso.php
+++ profil perso.php
@@ -21,6 +21,7 @@
 		$followers=$row['followers'];
 		$publications=$row['publications'];
 		$admin=$row['admin'];
+		$prive=$row['prive'];
 	?>
 
 	<form method="POST" action="">
@@ -29,8 +30,11 @@
 		<p>Followers:<?php echo "$followers";?></p>
 		<p>Publications:<?php echo "$publications";?></p>
 		<p>Admin:<?php if($admin==0){echo "Non";}else{echo "Oui";};?></p>
+		<p>Privé: <?php if($prive==false){echo "Non";}else{echo "Oui";}?></p>
 
 		<input type="submit" name="mdp" value="Changer le mot de passe"> 
+		<input type="submit" name="rpub" value="Rendre votre profil public">
+		<input type="submit" name="rpri" value="Rendre votre profil privé">
 	</form>
 	<?php
 
@@ -38,6 +42,27 @@
 			header("Location:modif mdp.php");
 			exit;
 		}
+
+		if(isset($_POST['rpub'])){
+			if($prive==false){
+				echo "<div class='erreur'>Votre profil est déjà public.</div>";
+			}else{
+				$up1="UPDATE profil SET prive=false WHERE pseudo='{$_SESSION['pseudo']}';";
+				$up1=mysqli_query($conn,$up1);
+				echo "<div class='succes'>Votre profil a été rendu public avec succès.</div>";
+			}
+		}
+
+		if(isset($_POST['rpri'])){
+			if($prive==true){
+				echo "<div class='erreur'>Votre profil est déjà privé.</div>";
+			}else{
+				$up1="UPDATE profil SET prive=true WHERE pseudo='{$_SESSION['pseudo']}';";
+				$up1=mysqli_query($conn,$up1);
+				echo "<div class='succes'>Votre profil a été rendu privé avec succès.</div>";
+			}
+		}
+	
 
 		?>
 
